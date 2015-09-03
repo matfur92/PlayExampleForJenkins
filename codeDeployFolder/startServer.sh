b03d62b8bca87f3f03ae7e58b7ca53f93bd1d692
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-09-03 12:31:59 +0200

    fix paths

--- codeDeployFolder/startServer.sh
+++ codeDeployFolder/startServer.sh
@@ -1,2 +1,3 @@
-unzip /target/universal/playexample*.zip -d /var/www
+#!/bin/bash
+unzip target/universal/playexample*.zip -d /var/www
 nohup /var/www/playexample*/bin/playexample -Dhttp.port=80 1>/dev/null 2>&1 &

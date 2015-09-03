96369396e5883d5534a1aae47af95d1f35aa5f5a
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-09-03 15:04:46 +0200

    fix script

--- codeDeployFolder/startServer.sh
+++ codeDeployFolder/startServer.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+sudo rm -Rf playexample-1.0-SNAPSHOT
 sudo unzip -o /var/www/target/universal/playexample*.zip -d /var/www
-sudo nohup /var/www/playexample*/bin/playexample -Dhttp.port=80 1>/dev/null 2>&1 &
+cd /
+sudo nohup /var/www/playexample*/bin/playexample -Dhttp.port=80 &

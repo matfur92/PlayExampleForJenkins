421664f98fc19b886fe0061da275b4c90e417252
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-09-03 15:20:14 +0200

    cleaning operations

--- codeDeployFolder/startServer.sh
+++ codeDeployFolder/startServer.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sudo rm -Rf playexample-1.0-SNAPSHOT
 sudo unzip -o /var/www/target/universal/playexample*.zip -d /var/www
-cd /
 sudo nohup /var/www/playexample*/bin/playexample -Dhttp.port=80 &
--- codeDeployFolder/stopServer.sh
+++ codeDeployFolder/stopServer.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo pkill -f java
 sudo sleep 10
+sudo cd /var/www
+sudo rm -Rf

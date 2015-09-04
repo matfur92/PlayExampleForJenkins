a01f3d519368f1808fe64b2ece9209908b3aa894
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-09-04 09:28:14 +0200

    Fix command to stop Play

--- codeDeployFolder/stopServer.sh
+++ codeDeployFolder/stopServer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sudo pkill -f java
+#sudo pkill -f java
+sudo pkill -f "sudo nohup /var/www/playexample-1.0-SNAPSHOT/bin/playexample -Dhttp.port=80"
 sudo sleep 10
 sudo rm -Rf /var/www/*
